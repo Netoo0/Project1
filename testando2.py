a293168196fe1f867563bed21dd42527dc0d90b5
Netoo0 <72075263+Netoo0@users.noreply.github.com>
2023-03-15 16:27:54 -0300

    Update testando2.py

--- testando2.py
+++ testando2.py
@@ -3,69 +3,70 @@ from PyQt5 import uic, QtWidgets
 import sqlite3
 
 
-class appdelogin():
+class AppDeLogin:
+    def __init__(self):
+        self.tela = uic.loadUi("Telainicial.ui")
+        self.tela2 = uic.loadUi("tela2.ui")
+        self.tela3 = uic.loadUi("tela3.ui")
+        
+        self.tela.pushButton.clicked.connect(self.login)
+        self.tela.pushButton_2.clicked.connect(self.abrir_tela_cadastro)
+        self.tela2.pushButton.clicked.connect(self.sair)
+        self.tela3.pushButton.clicked.connect(self.cadastrar_usuario)
+        
+        self.banco = sqlite3.connect('usuarios_cadastrados.db')
+        self.cursor = self.banco.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text, login text, senha text)")
+        
     def login(self):
-        usuario = tela.lineEdit.text()
-        senha = tela.lineEdit_2.text()
-        banco = sqlite3.connect('usuarios_cadastrados.db')
-        cursor = banco.cursor()
+        usuario = self.tela.lineEdit.text()
+        senha = self.tela.lineEdit_2.text()
+        
         try:
-            cursor.execute(
-                "SELECT senha FROM cadastro WHERE login ='{}'".format(usuario))
-            senhadb = cursor.fetchall()
-            banco.close()
-        except:
+            self.cursor.execute(
+                "SELECT senha FROM cadastro WHERE login = ?", (usuario,))
+            senhadb = self.cursor.fetchone()
+        except sqlite3.Error:
             print("Login não cadastrado no banco de dados")
-        if senha == senhadb[0][0]:
+            return
+        
+        if senhadb is not None and senha == senhadb[0]:
             print("logado")
-            tela.close()
-            tela2.show()
-        elif senha != senhadb[0][0]:
-            tela.label_4.setText("Dados incorretos!!")
-
-    def abrirtelacadastro(self):
-        tela.close()
-        tela3.show()
-
-    def cadastro(self):
-        nome = tela3.lineEdit.text()
-        login = tela3.lineEdit_2.text()
-        senha = tela3.lineEdit_3.text()
-        rpsenha = tela3.lineEdit_4.text()
-        if senha == rpsenha:
-            if any(x.isupper() for x in senha):
-                try:
-                    banco = sqlite3.connect('usuarios_cadastrados.db')
-                    cursor = banco.cursor()
-                    cursor.execute(
-                        "CREATE TABLE IF NOT EXISTS cadastro (nome text,login text,senha text)")
-                    cursor.execute("INSERT INTO cadastro VALUES ('" +
-                                nome+"','"+login+"','"+senha+"')")
-                    banco.commit()
-                    banco.close()
-                    tela3.label_6.setText("Você foi cadastrado com sucesso")
-                    tela3.close()
-                    tela.show()
-                except sqlite3.Error as erro:
-                    print("Erro ao inserir os dados: ", erro)
-            else:
-                tela3.label_6.setText("Você deve colocar letra maiuscula e minuscula")
+            self.tela.close()
+            self.tela2.show()
         else:
-            tela3.label_6.setText("As senhas digitadas estão diferentes")
+            self.tela.label_4.setText("Dados incorretos!!")
 
+    def abrir_tela_cadastro(self):
+        self.tela.close()
+        self.tela3.show()
+
+    def cadastrar_usuario(self):
+        nome = self.tela3.lineEdit.text()
+        login = self.tela3.lineEdit_2.text()
+        senha = self.tela3.lineEdit_3.text()
+        rpsenha = self.tela3.lineEdit_4.text()
+        
+        if senha == rpsenha and any(x.isupper() for x in senha):
+            try:
+                self.cursor.execute("INSERT INTO cadastro VALUES (?, ?, ?)", (nome, login, senha))
+                self.banco.commit()
+                self.tela3.label_6.setText("Você foi cadastrado com sucesso")
+                self.tela3.close()
+                self.tela.show()
+            except sqlite3.Error as erro:
+                print("Erro ao inserir os dados: ", erro)
+        else:
+            self.tela3.label_6.setText("As senhas digitadas estão diferentes ou não contém letra maiúscula e minúscula")
+            
     def sair(self):
-        tela2.close()
+        self.tela2.close()
+        
+    def executar(self):
+        self.tela.show()
+        QtWidgets.QApplication.instance().exec_()
 
 
-app = QtWidgets.QApplication([])
-tela = uic.loadUi("Telainicial.ui")
-tela2 = uic.loadUi("tela2.ui")
-tela3 = uic.loadUi("tela3.ui")
-tela.pushButton.clicked.connect(appdelogin.login)
-tela.pushButton_2.clicked.connect(appdelogin.abrirtelacadastro)
-tela2.pushButton.clicked.connect(appdelogin.sair)
-tela3.pushButton.clicked.connect(appdelogin.cadastro)
-
-
-tela.show()
-app.exec()
+if __name__ == '__main__':
+    app = AppDeLogin()
+    app.executar()
